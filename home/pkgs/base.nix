0045690732e8dfe3a83b06d043f274beec486bc5
gary <git@hyperion.me.uk>
2025-07-29 14:52:46 +0100

    add: pkgs

--- home/pkgs/base.nix
+++ home/pkgs/base.nix
@@ -11,16 +11,21 @@
     syncthing
     vscode-fhs
     inkscape
+    mpv
+    strawberry
 
     # utils
+    wget
+    curl
+    yt-dlp
+    file
+    p7zip
+    ffmpeg
+    imagemagick
     htop
     rclone
     whois
     fastfetch
     tree
-
-    # media
-    mpv
-    strawberry
   ];
 }
