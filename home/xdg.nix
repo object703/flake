4728c00daea412309f92340de6537ecabab81fd4
gary <git@hyperion.me.uk>
2025-07-29 14:52:16 +0100

    add: xdg config

---
+++ home/xdg.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+xdg.userDirs = {
+  enable = true;
+  createDirectories = true;
+  desktop = "\$HOME/desktop";
+  documents = "\$HOME/docs";
+  pictures = "\$HOME/pics";
+  videos = "\$HOME/videos";
+  download = "\$HOME/downloads";
+  publicShare = "\$HOME/share";
+}
