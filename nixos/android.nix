48f8406c5df309181dff0793532d3cf6228e0d1a
gary <git@hyperion.me.uk>
2025-12-11 21:25:07 +0000

    *

--- nixos/android.nix
+++ nixos/android.nix
@@ -1,7 +1,12 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs,  ... }:
 
 {
   programs.adb.enable = true;
   users.users.gary.extraGroups = [ "kvm" "adbusers" ];
-  environment.systemPackages = [ pkgs.android-studio ];
+  environment.systemPackages = [
+    pkgs.android-studio-full
+    pkgs.androidenv.androidPkgs.androidsdk
+    #pkgs.androidenv.androidPkgs.all.system-images.v36x.google_apis_playstore.x86_64
+  ];
+  nixpkgs.config.android_sdk.accept_license = true;
 }
